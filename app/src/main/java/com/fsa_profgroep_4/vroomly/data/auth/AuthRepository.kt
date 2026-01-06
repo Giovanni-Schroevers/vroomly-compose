ff6049a5e5e681bb381e8f29c12fb84dbedf8da9
Giovanni-Schroevers <giovie2000@hotmail.com>
2026-01-06 16:03:09 +0100

    Clean up AuthRepository

--- app/src/main/java/com/fsa_profgroep_4/vroomly/data/auth/AuthRepository.kt
+++ app/src/main/java/com/fsa_profgroep_4/vroomly/data/auth/AuthRepository.kt
@@ -19,50 +19,33 @@ class AuthRepositoryImpl(
     private val userDao: UserDao
 ) : AuthRepository {
     override suspend fun login(email: String, password: String): Result<LoginQuery.Login> {
-        Log.d(TAG, "login() called with email: $email")
         return runCatching {
-            Log.d(TAG, "Executing Apollo query...")
             val response = withContext(Dispatchers.IO) {
                 apolloClient.query(LoginQuery(email = email, password = password)).execute()
             }
-            Log.d(TAG, "Apollo query executed. hasErrors: ${response.hasErrors()}")
             
             if (response.hasErrors()) {
                 val errorMsg = response.errors?.first()?.message ?: "Unknown login error"
-                Log.e(TAG, "Login error: $errorMsg")
                 throw Exception(errorMsg)
             }
 
-            val loginData = response.data?.login
-            Log.d(TAG, "loginData: $loginData")
-            
-            if (loginData == null) {
-                Log.e(TAG, "Login data is null")
-                throw Exception("Login data is null")
-            }
-            
+            val loginData = response.data?.login ?: throw Exception("Login data is null")
+
             val user = loginData.user
-            Log.d(TAG, "user: $user, token: ${loginData.token?.take(20)}...")
-            
-            if (user != null && loginData.token != null) {
-                Log.d(TAG, "Saving user to database...")
-                userDao.clearTable()
-                userDao.insertUser(
-                    UserEntity(
-                        id = user.id,
-                        email = user.email,
-                        firstName = user.firstname,
-                        middleName = user.middleName,
-                        lastName = user.lastname,
-                        username = user.username,
-                        dateOfBirth = user.dateOfBirth.toString(),
-                        token = loginData.token
-                    )
+
+            userDao.clearTable()
+            userDao.insertUser(
+                UserEntity(
+                    id = user.id,
+                    email = user.email,
+                    firstName = user.firstname,
+                    middleName = user.middleName,
+                    lastName = user.lastname,
+                    username = user.username,
+                    dateOfBirth = user.dateOfBirth.toString(),
+                    token = loginData.token
                 )
-                Log.d(TAG, "User saved successfully!")
-            } else {
-                Log.w(TAG, "User or token is null, not saving to database")
-            }
+            )
             
             loginData
         }.also { result ->
