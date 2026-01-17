e06d52e5841d38edbfcd3f2ef132ae59275141b8
JasperRutte <jasperrutte@gmail.com>
2026-01-17 03:51:03 +0100

    fix blue background during actual mobile usage

--- app/src/main/java/com/fsa_profgroep_4/vroomly/MainActivity.kt
+++ app/src/main/java/com/fsa_profgroep_4/vroomly/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
